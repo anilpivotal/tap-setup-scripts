2e4e620f78fc0d13632624faf56b543a0e981ebc
Anil Nair <50462530+anilpivotal@users.noreply.github.com>
2021-11-27 19:37:33 -0500

    Update install-tce-prereqs.sh

--- install-tce-prereqs.sh
+++ install-tce-prereqs.sh
@@ -89,6 +89,7 @@ log "Installing Homebrew"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> $HOME/.profile
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+brew install gcc
 
 log "Installing TCE"
 brew install vmware-tanzu/tanzu/tanzu-community-edition
